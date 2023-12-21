5c58fd83a57e5d47a9f12674323259a30856995f
Yulian Melnychuk <julian95@o2.pl>
2023-12-21 15:26:15 +0100

    filter category

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/ui/button';
+import CategoryFilter from '@/components/ui/shared/CategoryFilter';
 import Collection from '@/components/ui/shared/Collection';
 import Search from '@/components/ui/shared/Search';
 import { getAllEvents } from '@/lib/actions/event.actions';
@@ -50,7 +51,7 @@ export default async function Home({ searchParams }: SearchParamProps) {
 
         <div className="flex w-full flex-col gap-5 md:flex-row">
           <Search />
-          {/* <CategoryFilter /> */}
+          <CategoryFilter />
         </div>
 
         <Collection
